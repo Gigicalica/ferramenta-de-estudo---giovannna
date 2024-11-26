8056681500daabb0f0edf61a3a699262960944c8
Gigicalica <00001117568933sp@al.educacao.sp.gov.br>
2024-11-25 21:59:10 -0300

    Add files via upload

---
+++ ferramenta de estudo - giovanna/perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Portugues',
+    'O que é pronome ',
+    'palavra que representa um nome, um termo us. com a função de um nome, um adjetivo ou toda uma oração que a segue ou antecede.'
+
+criaCartao(
+    'Geografia',
+    'o que é efeito estufa',
+    'palavra que representa um nome, um termo us. com a função de um nome, um adjetivo ou toda uma oração que a segue ou antecede.'
+)
+
+criaCartao(
+    'matematica',
+    'O que é bhaskara',
+    ' é um método resolutivo para equações do segundo grau cujo nome homenageia o grande matemático indiano que a demonstrou.'
+)
+
+criaCartao(
+    'sociologia',
+    'o que é democracia racial',
+    'é o estado de plena igualdade entre as pessoas independentemente de raça, cor ou etnia.'
+)
